99c253eefe773d5c2215a648eb3d8a725a1ac346
Blair Garrett <blair.garrett@sky.uk>
2017-12-16 23:58:46 +0000

    Fixed codacy issues

--- webpack.client.prod.config.js
+++ webpack.client.prod.config.js
@@ -1,3 +1,6 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-unused-vars */
+
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
